edf5a0129c3238bcd2dbce7373f00f335f2dd2b3
xtadeushx <alekscherkasov13@gmail.com>
2023-08-09 15:56:02 +0300

    fix: input.stories.tsx

--- src/components/common/input/input.stories.tsx
+++ src/components/common/input/input.stories.tsx
@@ -1,28 +1,33 @@
-import { Story, Meta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import '../../../index.css';
 import { Input } from "./input";
-import { TInputProps } from './types';
 
-export default {
+const meta: Meta<typeof Input> = {
   title: 'Components/Input',
   component: Input,
-} as Meta;
+};
 
-const Template: Story<TInputProps> = (args: TInputProps) => <Input {...args} />;
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+
 
 // Basic Input
-export const Text_Input = Template.bind({});
-Text_Input.args = {
-  text: 'Input',
-  placeholder: 'placeholder',
-  onChange: () => alert('Input changed'),
-  className: 'w-full py-1 px-2 my-2 mx-0 bg-white max-width-[200px] placeholder:opacity-40',
+export const Primary: Story = {
+  args: {
+    text: 'Input',
+    placeholder: 'placeholder',
+    onChange: () => alert('Input changed'),
+    className: 'w-full py-1 px-2 my-2 mx-0 bg-white max-width-[200px] placeholder:opacity-40',
+  },
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  text: 'Input',
-  placeholder: 'placeholder',
-  disabled: true,
-  className: 'w-full py-1 px-2 my-2 mx-0 bg-white max-width-[200px] placeholder:opacity-40',
+export const Disabled: Story = {
+  args: {
+    text: 'Input',
+    placeholder: 'placeholder',
+    disabled: true,
+    className: 'w-full py-1 px-2 my-2 mx-0 bg-white max-width-[200px] placeholder:opacity-40',
+  },
 };
+
